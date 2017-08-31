4c24c381f1b77dc78b8a4525f2993797838dac5c
Rythal <illustriousdreamer@gmail.com>
2017-08-30 21:30:37 -0400

    7.3 updates

--- NxWarehouse.lua
+++ NxWarehouse.lua
@@ -27,7 +27,7 @@
 
 local _G = getfenv(0)
 
-CarboniteWarehouse = LibStub("AceAddon-3.0"):NewAddon("CarboniteWarehouse","AceEvent-3.0","AceComm-3.0")
+CarboniteWarehouse = LibStub("AceAddon-3.0"):NewAddon("CarboniteWarehouse", "AceEvent-3.0", "AceComm-3.0")
 
 local L = LibStub("AceLocale-3.0"):GetLocale("Carbonite.Warehouse", true)
 
